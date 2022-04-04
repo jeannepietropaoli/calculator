19e431ac42ace4d81540db0ef34b25399b6a4435
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-04-04 08:52:12 -0400

    Make the calculator work: evaluate each pair nb

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const NUMBERBUTTONS = document.querySelectorAll('.number');
+const DISPLAYRESULT = document.querySelector('.result');
+const OPERATORBUTTONS = document.querySelectorAll('.operator');
+const EQUALBUTTON = document.querySelector('#equal');
+let value1 = '';
+let value2 = '';
+let operator;
+let operatorActive = false;
+let resultInStock=null;
+
 function add(a,b) {
     return a+b;
 }
@@ -26,3 +36,32 @@ function operate(operator,a,b){
             return divide(a,b);
     }
 }
+
+NUMBERBUTTONS.forEach(numberButton => {
+    numberButton.addEventListener('click', function displayNumbers() {
+        if (!operatorActive) {
+            value1 += numberButton.textContent;
+            DISPLAYRESULT.textContent = value1;
+        } else {
+            value2 += numberButton.textContent;
+            DISPLAYRESULT.textContent = value2;
+        }
+        OPERATORBUTTONS.forEach(operatorButton => {
+            operatorButton.addEventListener('click', function displayOperator(){
+                operatorActive = true;
+                operator = operatorButton.textContent;
+                DISPLAYRESULT.textContent = operator;
+                if (resultInStock) {
+                    value1 = resultInStock;
+                    value2 = '';
+                }
+            })
+        })
+        if (value1!=='' && value2!==''){
+            resultInStock = operate(operator, value1, value2);
+            EQUALBUTTON.addEventListener('click', function displayResult(){
+                DISPLAYRESULT.textContent = resultInStock;
+            })
+        }
+    })
+})
